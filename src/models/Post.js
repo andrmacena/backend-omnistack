399739f27503ff55395041e78a9d2d6e699c8600
André Macena <andrmacena_@outlook.com>
2019-06-24 21:32:12 -0300

    Clean code

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-    author: String,
-    place: String,
-    description: String,
-    hashtag: String,
-    image: String,
-    likes: {
-        type: Number,
-        default: 0
-    }
+   author: String,
+   place: String,
+   description: String,
+   hashtag: String,
+   image: String,
+   likes: {
+      type: Number,
+      default: 0
+   }
 },
-{ timestamps: true })
+   { timestamps: true })
 
 module.exports = mongoose.model('Post', PostSchema)
-
-
