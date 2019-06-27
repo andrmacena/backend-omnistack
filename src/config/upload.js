4a8ac2ce38cd907e1194c3cd41d63f594f7661c8
André Macena <andrmacena_@outlook.com>
2019-06-26 21:03:42 -0300

    Adiciona hash ao nome dos arquivo

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,12 +1,18 @@
 const multer = require('multer')
 const path = require('path')
+const crypto = require('crypto')
 
 module.exports = {
     storage: new multer.diskStorage({
         //resolve: estou pegando o diretorio raiz com _dirname, voltando duas pastas e entrando na pasta uplodas pra salvar ro arquivo
         destination: path.resolve(__dirname, '..','..', 'uploads'),
-        filename: function(req, file, callback){
-            callback(null, file.originalname)
+        filename: (req, file, callback) => {
+            crypto.randomBytes(16, (err,hash) =>{
+                if(err) callback(err)
+
+                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                callback(null, fileName)
+            })
         }
     })
 }
