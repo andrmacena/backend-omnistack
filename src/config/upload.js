1ece8e9579cfa86e9dab28235c356edc538026a1
André Macena <andrmacena_@outlook.com>
2019-06-26 22:58:18 -0300

    Update upload.js
    Definido os tipos de storage, sendo local e s3. S3 ainda não está totalmente configurado

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,18 +1,39 @@
 const multer = require('multer')
 const path = require('path')
 const crypto = require('crypto')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
+
+const storageTypes = {
+   local: new multer.diskStorage({
+      //resolve: estou pegando o diretorio raiz com _dirname, voltando duas pastas e entrando na pasta uplodas pra salvar ro arquivo
+      destination: path.resolve(__dirname, '..', '..', 'uploads'),
+      filename: (req, file, callback) => {
+         crypto.randomBytes(16, (err, hash) => {
+            if (err) callback(err)
+
+            const fileName = `${hash.toString('hex')}-${file.originalname}`
+            callback(null, fileName)
+         })
+      }
+   }),
+   s3: multerS3({
+      //lê minhas variaveis ambiente, para configuração de produção
+      s3: new aws.S3(),
+      bucket: 'backend-semanaomnistack',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      acl: 'public-read',
+      key: (req, file, cb) => {
+         crypto.randomBytes(16, (err, hash) => {
+            if (err) cb(err)
+
+            const fileName = `${hash.toString('hex')}-${file.originalname}`
+            cb(null, fileName)
+         })
+      }
+   }),
+}
 
 module.exports = {
-    storage: new multer.diskStorage({
-        //resolve: estou pegando o diretorio raiz com _dirname, voltando duas pastas e entrando na pasta uplodas pra salvar ro arquivo
-        destination: path.resolve(__dirname, '..','..', 'uploads'),
-        filename: (req, file, callback) => {
-            crypto.randomBytes(16, (err,hash) =>{
-                if(err) callback(err)
-
-                const fileName = `${hash.toString('hex')}-${file.originalname}`
-                callback(null, fileName)
-            })
-        }
-    })
+    storage: storageTypes["local"]
 }
