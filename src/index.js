003a8bfae49c84cc15a93d7c6f5bf17bd4e51b0c
André Macena <andrmacena_@outlook.com>
2019-06-28 21:22:36 -0300

    Update index.js
    Adicionado url da API online, disponibilizada no Heroku

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 app.use(cors())
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+app.use('/files', express.static('https://backend-semanaomnistack.herokuapp.com'))
 
 app.use(require('./routes'))
 
