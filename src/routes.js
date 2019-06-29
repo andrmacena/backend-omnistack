6ac97dc731f884da22e35362407724da997e24dc
André Macena <andrmacena_@outlook.com>
2019-06-28 21:43:13 -0300

    Update routes.js
    Adicionado rota para deleçao de posts

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const upload = multer(uploadConfig)
 
 routes.get('/posts', PostController.index)
 routes.post('/posts', upload.single('image'), PostController.store)
+routes.delete('/posts/:id', PostController.delete)
 
 routes.post('/posts/:id/like', LikeController.store)
 
