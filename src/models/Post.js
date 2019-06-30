940cd628d74b4eaa77f24d84f86cfcc9479b6743
André Macena <andrmacena_@outlook.com>
2019-06-29 22:42:19 -0300

    Update Post.js
    Adicionar URL para imagens salvas localmente, utilizando middleware antes da imagem ser salva no AWS

--- src/models/Post.js
+++ src/models/Post.js
@@ -15,4 +15,10 @@ const PostSchema = new mongoose.Schema({
 },
    { timestamps: true })
 
+PostSchema.pre('save', function () {
+   if (!this.url) {
+      this.url = `${process.env.APP_URL}/files/${this.key}`
+   }
+})
+
 module.exports = mongoose.model('Post', PostSchema)
